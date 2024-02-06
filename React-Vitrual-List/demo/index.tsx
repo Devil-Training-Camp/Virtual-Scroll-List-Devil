5e5678bba66c2216b137382a74c16814c3bded69
open1i <open160405103@163.com>
2024-02-06 18:15:52 +0800

    react虚拟滚动列表组件

---
+++ React-Vitrual-List/demo/index.tsx
@@ -0,0 +1,40 @@
+import { createRoot } from 'react-dom/client';
+
+import  VirtualList, { DIRECTION } from '../src';
+import type { ItemStyle } from '../src';
+import './index.css';
+
+const EXAMPLE = () => {
+  const renderItem = ({style, index}: {style: ItemStyle; index: number}) => {
+    return (
+      <div className="Row" style={style} key={index}>
+        Row #{index}
+      </div>
+    );
+  };
+
+  const getItemSize = (_: number) => {
+    return (Math.random() * 50) + 50
+  }
+  
+  return (
+    <div className="Root">
+      <VirtualList
+        width={800}
+        height={400}
+        itemCount={300}
+        renderItem={renderItem}
+        itemSize={getItemSize}
+        className="VirtualList"
+        estimatedItemSize={50}
+        // scrollToIndex={12}
+        scrollDirection={DIRECTION.VERTICAL}
+        // scrollDirection={DIRECTION.HORIZONTAL}
+      />
+    </div>
+  );
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<EXAMPLE />);
